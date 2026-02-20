8e4af67ad3fca8c3412dd048be85869a471f74c9
한성호 <hoya@Hoya-MacBook-Air.local>
2026-02-20 18:30:33 +0900

    fix: 여백정리

--- src/common/dto/base-response.dto.ts
+++ src/common/dto/base-response.dto.ts
@@ -2,18 +2,18 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export abstract class BaseResponseDto {
-    @ApiProperty({ example: 1, description: '고유 ID' })
-    id: string;
+  @ApiProperty({ example: 1, description: '고유 ID' })
+  id: string;
 
-    @ApiProperty({ example: '2023-10-26T09:55:00.000Z', description: '생성 시간' })
-    createdAt: Date;
+  @ApiProperty({ example: '2023-10-26T09:55:00.000Z', description: '생성 시간' })
+  createdAt: Date;
 
-    @ApiProperty({ example: '2023-10-26T09:55:00.000Z', description: '마지막 업데이트 시간' })
-    updatedAt: Date;
+  @ApiProperty({ example: '2023-10-26T09:55:00.000Z', description: '마지막 업데이트 시간' })
+  updatedAt: Date;
 
-    constructor(entity: any) {
-        this.id = entity.id
-        this.createdAt = entity.createdAt
-        this.updatedAt = entity.updatedAt
-    }
+  constructor(entity: any) {
+      this.id = entity.id
+      this.createdAt = entity.createdAt
+      this.updatedAt = entity.updatedAt
+  }
 }
--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -3,16 +3,16 @@ import { ApiProperty } from "@nestjs/swagger";
 import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 export abstract class BaseEntity {
-    @ApiProperty({ example: 1, description: '고유 ID' })
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @ApiProperty({ example: 1, description: '고유 ID' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @CreateDateColumn({ type: 'timestamptz' })
-    createdAt: Date;
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
 
-    @UpdateDateColumn({ type: 'timestamptz'})
-    updatedAt: Date;
+  @UpdateDateColumn({ type: 'timestamptz'})
+  updatedAt: Date;
 
-    @DeleteDateColumn({ type: 'timestamptz', nullable: true })
-    deletedAt: Date | null;
+  @DeleteDateColumn({ type: 'timestamptz', nullable: true })
+  deletedAt: Date | null;
 }
